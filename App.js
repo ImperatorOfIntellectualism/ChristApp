4099584b0a9aae4acdd0979d85db3bd77d4a2b20
King of Kings <Bachuganchikomania2@gmail.com>
2021-08-16 16:58:36 +0700

    initial

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { ScrollView, Text, View } from "react-native";
+import styled from "styled-components/native";
+import Group from "./components/Group";
+import {Ionicons} from '@expo/vector-icons'
+
+const Data = { 
+  Group1: {
+  groupTitle: "14 Сентября",
+        items: [
+          {
+            time: "15:00",
+            uri: "https://e1.pngegg.com/pngimages/411/363/png-clipart-goku-dbs-son-goku-thumbnail.png",
+            fullName: "AAAIIIEEE",
+            subText: "JOBBER",
+            active: true,
+          },
+          {
+            time: "16:00",
+            uri: "https://64.media.tumblr.com/e3544a46f7798594a2782ed4fd9574a7/eb916997e38a5008-12/s640x960/1f87b735cfa10d1c68e2a02689a1ed0ac9a1335f.jpg",
+            fullName: "CHAD",
+            subText: "GOD",
+            active: false,
+          },
+        ],
+      },
+  Group2:{
+    groupTitle: "17 Сентября",
+        items: [
+          {
+            time: "15:00",
+            uri: "https://e1.pngegg.com/pngimages/411/363/png-clipart-goku-dbs-son-goku-thumbnail.png",
+            fullName: "AAAIIIEEE",
+            subText: "JOBBER",
+            active: true,
+          },
+          {
+            time: "16:00",
+            uri: "https://64.media.tumblr.com/e3544a46f7798594a2782ed4fd9574a7/eb916997e38a5008-12/s640x960/1f87b735cfa10d1c68e2a02689a1ed0ac9a1335f.jpg",
+            fullName: "CHAD",
+            subText: "GOD",
+            active: false,
+          },
+        ],
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Container>
+      <ScrollView>
+      <Group
+        groupTitle={Data.Group1.groupTitle}
+        items={Data.Group1.items}
+      />
+      <Group
+        groupTitle={Data.Group2.groupTitle}
+        items={Data.Group2.items}
+      />
+      <Group
+        groupTitle={Data.Group2.groupTitle}
+        items={Data.Group2.items}
+      />
+      <Group
+        groupTitle={Data.Group2.groupTitle}
+        items={Data.Group2.items}
+      />
+      <Group
+        groupTitle={Data.Group2.groupTitle}
+        items={Data.Group2.items}
+      />
+      <Group
+        groupTitle={Data.Group2.groupTitle}
+        items={Data.Group2.items}
+      />
+      </ScrollView>
+      <PlusButton>
+        <Ionicons name="add-circle-outline" size={38} color="black" />
+      </PlusButton>
+    </Container>
+  ); //1:23
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  margin-top: 50px;
+`;
+
+const PlusButton = styled.TouchableOpacity`
+position: absolute;
+bottom: 5%;
+right: 5%;
+align-items: center;
+justify-content: center;
+border-radius: 50px;
+width: 64px;
+height:64px;
+background: #2A86FF;
+`;
---
+++ components/Group.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styled from 'styled-components/native'
+
+const Group = ({groupTitle, items }) => {
+  console.log(items)
+return(
+<GroupContainer>
+        <GroupTitle>
+          {groupTitle}
+        </GroupTitle>
+            {items.map(item => 
+        <GroupItem>
+          <Avatar source={{uri: item.uri}}>
+          </Avatar>
+          <View style={{paddingLeft: 10, flex: 1}}>
+          <FullName>{item.fullName}</FullName>
+          <GrayText>{item.subText}</GrayText>
+          </View>
+          <GroupData active={item.active}>{item.time}</GroupData>
+        </GroupItem>)}
+      </GroupContainer>
+)
+}
+
+Group.defaultProps = {
+  groupTitle: 'Untitled',
+  items: []
+};
+
+
+
+const Avatar = styled.Image`
+  border-radius: 50px;
+  width: 40px;
+  height: 40px;
+`;
+
+const FullName = styled.Text`
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+const GroupItem = styled.TouchableOpacity`
+padding: 20px 0;
+flex-direction: row;
+align-items: center;
+borderBottomWidth: 2px;
+`;
+
+const GroupData = styled.Text`
+background: ${props => props.active ? "#2A86FF;" : "#AFE2F0;"};
+padding: 4px;
+border-radius: 18px;
+font-weight: 600;
+color: ${props => props.active ? "#FFFFFF;" : "#00499C;"};;
+font-size: 15px;
+width: 70px;
+height: 32px;
+text-align: center;
+line-height; 28px;
+`;
+
+const GrayText = styled.Text`
+  font-size: 14px;
+  color: #C0C0C0;
+`;
+
+const GroupTitle = styled.Text`
+  font-weight: 800;
+  font-size: 22px;
+  color: #000000;
+`;
+
+const GroupContainer = styled.View`
+padding: 0 20px;
+margin-bottom: 15px;
+`;
+
+export default Group;
